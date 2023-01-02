6369337ae15a7d45dca676e731dfc718debd8117
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2023-01-02 05:15:44 +0000

    done

--- set02/problem03.c
+++ set02/problem03.c
@@ -7,6 +7,10 @@ void output(int n, int result);
 int main() {
   int n,result;
   n=input_number();
+  if((n==0)||(n==1)) {
+    printf("%d is neither prime nor composite\n",n);
+    exit(0);
+  }
   result=is_composite(n);
   output(n,result);
   return 0;
@@ -24,19 +28,16 @@ int input_number() {
 }
 int is_composite(int n) {
   int i, count=0;
-  for(i=1;i<n;i++)
+  for(i=1;i<n/2;i++)
    if(n%i == 0){
      count = count+1;
    }
   return count;
   }
 void output(int n, int result) {
-  if(n==1){
-       printf("1 is neihter prime or composite"); 
-  }
-  else if(result==2)
-    printf("%d is a composite number\n",n);
-  else
+  if(result==1)
     printf("%d is a prime number\n",n);
+  else
+    printf("%d is a composite number\n",n);
 }
   
