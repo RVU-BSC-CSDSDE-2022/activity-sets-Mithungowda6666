c155aafccc9225749521ec6393d6650e4baa4df6
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2023-01-11 16:30:00 +0000

    about to solve

--- set02/problem06.c
+++ set02/problem06.c
@@ -6,17 +6,17 @@ void output(char *a, char *reverse_a);
 void input_string(char *a)
 {
   print("Enter the first string ");
-  scanf("%s", a);
+  scanf("%s",&a);
 }
 void str_reverse(char *str, char *rev_str)
 {
   int i,l=strlen(str)
-  char str[50],c;
+  char str[50],str;
   for(i=0;i<l/2;i++)
     {
-      c=str[i];
-      str[i]=
-        
+      str=str[i];
+      str[i] = str[l-1-i];
+      str[l-1-i] = str;
     }
 }
   
