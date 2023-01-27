2021a7a00f746b58b1569e16995abede3c4f460c
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2023-01-27 13:43:23 +0000

    changed logic

--- set01/problem10.c
+++ set01/problem10.c
@@ -8,8 +8,10 @@ int main(){
   char string1[100],string2[100];
   int result = 0;
   input_two_strings(string1,string2);
+  
   result = stringcompare(string1,string2);
   output(string1,string2,result);
+  return 0;
 }
 
 void input_two_strings(char *string1, char *string2){
@@ -17,6 +19,7 @@ void input_two_strings(char *string1, char *string2){
   scanf("%s", string1);
   printf("Enter the second string ");
   scanf("%s", string2);
+
   }
 
 int stringcompare(char *string1, char *string2)
@@ -29,6 +32,7 @@ int stringcompare(char *string1, char *string2)
         flag=1;
         break;
       }
+      i++;
     }
   if(flag==0)
   {  if(string1[i]=='\0' && string2[i]=='\0')
@@ -45,7 +49,7 @@ int stringcompare(char *string1, char *string2)
     else
       return 2;
   }
-
+  return(flag);
 }
 
 void output(char *string1, char *string2, int result){
