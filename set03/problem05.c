f7737b52630cb2707e5cd6721478a873a3fac11d
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2023-01-30 14:08:40 +0000

    abt

--- set03/problem05.c
+++ set03/problem05.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
+#include<math.h>
 int input_array_size();
 void init_array(int n, int a[n]);
 void erotosthenes_sieve(int n, int a[n]);
 void output(int n, int a[n]);
 
+int main()
+{
+  int n;
+  n=input_array_size();
+  int a[n];
+  init_array(n, a);
+  erotosthenes_sieve(n,a);
+  output(n,a);
+  return 0;
+}
 int input_array_size()
 {
   int a;
@@ -21,16 +32,21 @@ void init_array(int n, int a[n])
 }
 void erotosthenes_sieve(int n, int a[n])
 {
+  int i,j;
   for(i=2;i<=sqrt(n);i++)
     {
-      if(a==2)
-        for(j=i*2;j<n;j++)
+      if(a[i]!=0)
+        for(j=pow(a[i],2);j<n;j=j+a[i])
           {
-            a[i]=-1;
+            a[j-1]=0;
           }
     }
 }
 void output(int n, int a[n])
-{
-  printf("The output is %d")
+{ int i;
+  for(i=0;i<n;i++)
+    { 
+      if(a[i]!=0)
+      printf("The output is %d,\t",a[n]);
+    }
 }
