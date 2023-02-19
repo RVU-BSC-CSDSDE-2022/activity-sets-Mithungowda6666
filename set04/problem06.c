01bc7bf094428c85960bb2aeb70e084f3ce9229d
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2023-02-19 12:59:25 +0000

    solved

---
+++ set04/problem06.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char *a);
+int count_words(char *string);
+void output(char *string, int no_words);
+
+void input_string(char *a)
+{
+  
+}
--- set04/problem07.c
+++ set04/problem07.c
@@ -17,14 +17,7 @@ Fraction input_fraction()
   printf("Enter the value of the denominator ");
   scanf("%d",&f.den);
 }
-Fraction add_fractions(Fraction f1, Fraction f2)
-{
-  int a,b;
-   a=(f1.num*f2.den)+(f1.den*f2.num);
-   b= f1.den*f2.den;
-   
-   return(a,b);
-}
+
 int find_gcd(int a, int b)
 {
   int i,gcd;
@@ -34,6 +27,14 @@ int find_gcd(int a, int b)
       {gcd=i;}
     }
   return(gcd);
+}
+Fraction add_fractions(Fraction f1, Fraction f2)
+{
+  Fraction sum;
+  int a = (f1.den*f2.den)/find_gcd(f1.den,f2.den);
+  sum.num = (a/f1.den) * f1.num + (a/f2.den)*f2.num;
+  sum.den = a;
+   return(sum);
 }
 void output(Fraction f1, Fraction f2, Fraction sum)
 {
