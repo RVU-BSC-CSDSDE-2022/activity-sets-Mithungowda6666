09cb7074bdee7d6d52007b8d4f07dae18758fd1d
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2023-02-20 13:01:50 +0000

    solved

--- set04/problem07.c
+++ set04/problem07.c
@@ -9,34 +9,47 @@ int find_gcd(int a, int b);
 Fraction add_fractions(Fraction f1, Fraction f2);
 void output(Fraction f1, Fraction f2, Fraction sum);
 
+int main()
+{
+ Fraction f1,f2,sum;
+ f1 = input_fraction();
+ f2 = input_fraction();
+ sum =  add_fractions(f1,f2);
+ output(f1,f2,sum);
+ return 0;
+}
+
 Fraction input_fraction()
 {
   Fraction f;
-  printf("Enter the value of the numerator ");
+  printf("Enter the value of the Numerator  ");
   scanf("%d",&f.num);
   printf("Enter the value of the denominator ");
   scanf("%d",&f.den);
+  return f;
 }
 
 int find_gcd(int a, int b)
 {
-  int i,gcd;
-  for(i=1;i<=a && i<=b;i++)
+  while(b!=0)
     {
-      if(a%i==0 && b%i==0)
-      {gcd=i;}
+      int temp = b;
+      b = a%b;
+      a = temp;
     }
-  return(gcd);
+  return a;
 }
+
 Fraction add_fractions(Fraction f1, Fraction f2)
 {
   Fraction sum;
-  int a = (f1.den*f2.den)/find_gcd(f1.den,f2.den);
-  sum.num = (a/f1.den) * f1.num + (a/f2.den)*f2.num;
-  sum.den = a;
-   return(sum);
+  int lcm =(f1.den*f2.den)/find_gcd(f1.den,f2.den);
+  sum.num = (lcm/f1.den)*f1.num + (lcm/f2.den)*f2.num;
+  sum.den = lcm;
+  return(sum);
 }
+
 void output(Fraction f1, Fraction f2, Fraction sum)
 {
-  printf("%d/%d + %d/%d = %d/%d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
+  printf("%d/%d +%d/%d = %d/%d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
 }
